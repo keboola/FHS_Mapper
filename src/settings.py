054fdd26e3c5129e1354bf58bb604fb3f56508a3
Kritiga Ravishankar <44443593+kritiga9@users.noreply.github.com>
2023-05-12 14:05:45 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ KEBOOLA_TOKEN = st.secrets["kbc_token"]
 keboola_client = Client(KEBOOLA_STACK, KEBOOLA_TOKEN)
 
 #STATUS_TAB_ID = 'out.c-create_configs.status'
-STATUS_TAB_ID = 'in.c-StreamlitIO.status'
+STATUS_TAB_ID = 'in.c-StreamlitIO.dev_status'
 MAPPING_CLASSES_TAB_ID = 'in.c-StreamlitIO.mapping_classes'
 STREAMLIT_BUCKET_ID = 'in.c-StreamlitIO'
 RESTAURANTS_TAB_ID = 'in.c-StreamlitIO.restaurant_aux'
